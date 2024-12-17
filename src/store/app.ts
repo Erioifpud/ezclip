75355fa945dc312b9f9f9e1d8f5ed160a622a24a
Erioifpud <erioifpud.cn@gmail.com>
2024-12-18 03:16:54 +0800

    feat: 实现 app store 雏形

---
+++ src/store/app.ts
@@ -0,0 +1,28 @@
+import { proxy } from "valtio";
+
+export interface Source {
+  name: string;
+  url: string;
+}
+
+export type Theme = 'light' | 'dark' | 'system';
+
+export type Color = 'zinc' | 'red' | 'rose' | 'orange' | 'green' | 'blue' | 'yellow' | 'violet'
+
+export interface AppState {
+  // 插件来源
+  sources: Source[];
+  // 禁用脚本的站点
+  blackList: string[];
+  // 主题
+  theme: Theme;
+  // 颜色
+  color: Color;
+}
+
+export const appStore = proxy<AppState>({
+  sources: [],
+  blackList: [],
+  theme: 'light',
+  color: 'blue',
+});
