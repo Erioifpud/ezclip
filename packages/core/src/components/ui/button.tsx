5be4605016596973c635b738cd2983e9134afde7
Erioifpud <erioifpud.cn@gmail.com>
2024-12-23 21:27:34 +0800

    fix: 修复 tw 前缀导致 shadcn 组件样式失效的问题

--- packages/core/src/components/ui/button.tsx
+++ packages/core/src/components/ui/button.tsx
@@ -5,25 +5,25 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "ec-inline-flex ec-items-center ec-justify-center ec-gap-2 ec-whitespace-nowrap ec-rounded-md ec-text-sm ec-font-medium ec-ring-offset-background ec-transition-colors focus-visible:ec-outline-none focus-visible:ec-ring-2 focus-visible:ec-ring-ring focus-visible:ec-ring-offset-2 disabled:ec-pointer-events-none disabled:ec-opacity-50 [&_svg]:ec-pointer-events-none [&_svg]:ec-size-4 [&_svg]:ec-shrink-0",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "ec-bg-primary ec-text-primary-foreground hover:ec-bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "ec-bg-destructive ec-text-destructive-foreground hover:ec-bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "ec-border ec-border-input ec-bg-background hover:ec-bg-accent hover:ec-text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "ec-bg-secondary ec-text-secondary-foreground hover:ec-bg-secondary/80",
+        ghost: "hover:ec-bg-accent hover:ec-text-accent-foreground",
+        link: "ec-text-primary ec-underline-offset-4 hover:ec-underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "ec-h-10 ec-px-4 ec-py-2",
+        sm: "ec-h-9 ec-rounded-md ec-px-3",
+        lg: "ec-h-11 ec-rounded-md ec-px-8",
+        icon: "ec-h-10 ec-w-10",
       },
     },
     defaultVariants: {
