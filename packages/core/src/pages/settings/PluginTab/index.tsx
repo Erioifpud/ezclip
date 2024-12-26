8e53ce04354a86ed784d855093fc3eca89cad6b7
Erioifpud <erioifpud.cn@gmail.com>
2024-12-26 19:43:58 +0800

    feat: 实现插件管理雏形

--- packages/core/src/pages/settings/PluginTab/index.tsx
+++ packages/core/src/pages/settings/PluginTab/index.tsx
@@ -1,17 +1,91 @@
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { PlugIcon } from "lucide-react"
-import { memo } from "react"
+import { SquarePenIcon, PlugIcon, ChevronUpIcon, ChevronDownIcon } from "lucide-react"
+import { Fragment, memo, useCallback, useEffect, useState } from "react"
 import { TitleBar } from "../components/TitleBar"
+import { useSnapshot } from "valtio"
+import { pluginStore } from "@/store/plugin"
+import { Button } from "@/components/ui/button"
+import { Plugin } from "@/store/plugin"
+
+interface PluginExtra extends Plugin {
+  expanded: boolean
+  permissions: string[]
+}
 
 export const PluginTab = memo(() => {
+  const pluginState = useSnapshot(pluginStore)
+
+  const [plugins, setPlugins] = useState<PluginExtra[]>([])
+
+  useEffect(() => {
+    // @ts-ignore
+    setPlugins(pluginState.installedPlugins.map(plugin => ({
+      ...plugin,
+      expanded: false,
+    })))
+  }, [pluginState.installedPlugins])
+
+  const togglePluginExpanded = useCallback((namespace: string) => {
+    const pluginIndex = plugins.findIndex(plugin => plugin.namespace === namespace)
+    if (pluginIndex !== -1) {
+      const newPlugins = [...plugins]
+      newPlugins[pluginIndex].expanded = !newPlugins[pluginIndex].expanded
+      setPlugins(newPlugins)
+    }
+  }, [plugins])
+
   return (
     <div className="ec-flex ec-flex-col ec-gap-4 ec-h-full">
       {/* 标题栏 */}
       <TitleBar title="插件管理" icon={<PlugIcon className="ec-w-4 ec-h-4" />} />
       {/* 内容区 */}
-      <ScrollArea className="ec-flex ec-flex-col ec-gap-4 ec-overflow-y-auto ec-px-2" >
+      <div data-ec-scrollbar className="ec-flex ec-flex-col ec-gap-4 ec-overflow-y-auto ec-px-2">
+        <div className="ec-flex ec-flex-col ec-gap-2">
+          {plugins.map((plugin) => (
+            <Fragment key={plugin.namespace}>
+              <div
+                className="ec-flex ec-items-center ec-justify-between ec-border ec-p-2 ec-rounded-lg"
+              >
+                <div className="ec-flex ec-flex-col ec-flex-grow ec-min-w-0">
+                  <span className="ec-font-semibold ec-tracking-tight ec-text-card-foreground ec-truncate">{plugin.name}</span>
+                  <span className="ec-text-muted-foreground ec-truncate ec-text-sm" title={plugin.description}>{plugin.description}</span>
+                </div>
+                {/* 展开状态按钮 */}
+                <Button
+                  className="ec-border ec-border-transparent ec-flex-shrink-0 ec-ml-2"
+                  variant="default"
+                  size="sm"
+                  onClick={() => {
+                    togglePluginExpanded(plugin.namespace)
+                  }}
+                >
+                  {plugin.expanded
+                    ? <ChevronUpIcon className="ec-w-4 ec-h-4" />
+                    : <ChevronDownIcon className="ec-w-4 ec-h-4" />}
+                </Button>
+                {/* 编辑按钮 */}
+                <Button
+                  className="ec-border ec-border-transparent ec-flex-shrink-0 ec-ml-2"
+                  variant="default"
+                  size="sm"
+                  onClick={() => {
+                    // TODO: 弹窗显示动态表单
+                  }}
+                >
+                  <SquarePenIcon className="ec-w-4 ec-h-4" />
+                </Button>
+              </div>
 
-      </ScrollArea>
+              {plugin.expanded && (
+                <div className="ec-flex ec-flex-col ec-gap-2">
+                  {plugin.actions.map(action => (
+                    <div key={action.id}>{action.name}</div>
+                  ))}
+                </div>
+              )}
+            </Fragment>
+          ))}
+        </div>
+      </div>
     </div>
   )
 })
