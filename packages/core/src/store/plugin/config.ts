70a94f1eaa1c08fd99616e44844292762caeb029
Erioifpud <erioifpud.cn@gmail.com>
2025-01-21 00:07:30 +0800

    feat: 补全插件配置类型描述

--- packages/core/src/store/plugin/config.ts
+++ packages/core/src/store/plugin/config.ts
@@ -1,11 +1,12 @@
-interface BaseField {
+interface BaseField<T> {
   label: string;
   name: string;
   required?: boolean;
-  validator?: (value: any) => boolean | string;
+  description?: string;
+  validator?: (value: T) => boolean | string;
 }
 
-interface TextField extends BaseField {
+interface TextField extends BaseField<string> {
   type: 'text';
   defaultValue?: string;
   props?: {
@@ -15,8 +16,29 @@ interface TextField extends BaseField {
   };
 }
 
-interface NumberField extends BaseField {
+interface TextareaField extends BaseField<string> {
+  type: 'textarea';
+  defaultValue?: string;
+  props?: {
+    placeholder?: string;
+    maxLength?: number;
+    minLength?: number;
+    rows?: number;
+  };
+}
+
+interface NumberField extends BaseField<number> {
   type: 'number';
+  defaultValue?: number;
+  props?: {
+    placeholder?: string;
+    min?: number;
+    max?: number;
+  };
+}
+
+interface SliderField extends BaseField<number> {
+  type: 'slider';
   defaultValue?: number;
   props?: {
     placeholder?: string;
@@ -26,26 +48,72 @@ interface NumberField extends BaseField {
   };
 }
 
-interface SelectField extends BaseField {
+interface SelectField extends BaseField<string> {
   type: 'select';
-  defaultValue?: any;
+  defaultValue?: string;
   options: Array<{
     label: string;
     value: any;
   }>;
   props?: {
-    multiple?: boolean;
-    searchable?: boolean;
+    placeholder?: string;
   };
 }
 
-interface SwitchField extends BaseField {
+interface SwitchField extends BaseField<boolean> {
   type: 'switch';
   defaultValue?: boolean;
 }
 
-export type PluginFormField = 
-  | TextField 
-  | NumberField 
-  | SelectField 
-  | SwitchField;
+interface DateField extends BaseField<string> {
+  type: 'date';
+  defaultValue?: string;
+}
+
+interface ColorField extends BaseField<string> {
+  type: 'color';
+  defaultValue?: string;
+}
+
+interface ListField extends BaseField<Array<string>> {
+  type: 'list';
+  defaultValue?: Array<string>;
+  props?: {
+    placeholder?: string;
+    // 字符数限制
+    minLength?: number;
+    maxLength?: number;
+    // 数组长度限制
+    min?: number;
+    max?: number;
+  };
+}
+
+export type PluginFormField =
+  | TextField
+  | NumberField
+  | SelectField
+  | SwitchField
+  | SliderField
+  | ListField
+  | TextareaField
+  | DateField
+  | ColorField;
+
+type TypeMap = {
+  text: string;
+  number: number;
+  select: string;
+  switch: boolean;
+  slider: number;
+  list: string[];
+  textarea: string;
+  date: string;
+  color: string;
+};
+
+export type FormValueType<T extends readonly PluginFormField[]> = {
+  [K in T[number]['name']]: Extract<T[number], { name: K }>['type'] extends keyof TypeMap
+    ? TypeMap[Extract<T[number], { name: K }>['type']]
+    : never;
+}
--- packages/core/src/store/plugin/utils.ts
+++ packages/core/src/store/plugin/utils.ts
@@ -1,3 +1,4 @@
+import { FormValueType } from './config';
 import { Action, Plugin, pluginStore } from './index';
 
 export function getActionId(action: Action): string {
@@ -6,14 +7,14 @@ export function getActionId(action: Action): string {
 
 // 判断插件是否"启用"（至少有一个 action 被启用）
 export function isPluginEnabled(plugin: Plugin): boolean {
-  return plugin.actions.some(action => 
+  return plugin.actions.some(action =>
     pluginStore.enabledActions.includes(`${plugin.namespace}.${action.id}`)
   );
 }
 
 // 获取插件已启用的 actions 数量
 export function getEnabledActionCount(plugin: Plugin): number {
-  return plugin.actions.filter(action => 
+  return plugin.actions.filter(action =>
     pluginStore.enabledActions.includes(`${plugin.namespace}.${action.id}`)
   ).length;
 }
@@ -21,7 +22,7 @@ export function getEnabledActionCount(plugin: Plugin): number {
 // 启用/禁用插件的所有 actions
 export function togglePluginActions(plugin: Plugin, enabled: boolean) {
   const actionIds = plugin.actions.map(action => `${plugin.namespace}.${action.id}`);
-  
+
   if (enabled) {
     pluginStore.enabledActions = [...new Set([
       ...pluginStore.enabledActions,
@@ -32,4 +33,40 @@ export function togglePluginActions(plugin: Plugin, enabled: boolean) {
       id => !actionIds.includes(id)
     );
   }
-} 
+}
+
+// 根据插件的配置创建初始表单值
+export function initPluginConfig(plugin: Plugin): FormValueType<typeof plugin.configForm> {
+  return plugin.configForm?.reduce((acc, field) => {
+    if (field.defaultValue !== undefined) {
+      acc[field.name] = field.defaultValue;
+      return acc;
+    }
+    // 如果没有 defaultValue，则根据 type 推断默认值
+    switch (field.type) {
+      case 'text':
+      case 'textarea':
+      case 'color':
+        acc[field.name] = '';
+        break;
+      case 'number':
+      case 'slider':
+        acc[field.name] = 0;
+        break;
+      case 'switch':
+        acc[field.name] = false;
+        break;
+      case 'select':
+        // 使用第一个选项作为默认值
+        acc[field.name] = field.options[0]?.value ?? '';
+        break;
+      case 'list':
+        acc[field.name] = [];
+        break;
+      case 'date':
+        acc[field.name] = new Date();
+        break;
+    }
+    return acc;
+  }, {} as Record<string, any>);
+}
